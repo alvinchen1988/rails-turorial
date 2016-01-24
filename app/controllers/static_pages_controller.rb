317aab8a3df60b73fbf1b7136737133d989fb841
Alvin Chen <alvinchen1988@icloud.com>
2016-01-23 23:34:54 -0800

    finish chap5 -- layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-=begin
-
-this
-is a 
-multiple
-line
-comment
-in ruby!
-
-=end
   end
-
+  
   def help
   end
 
   def about
+  end
+
+  def contact
   end
 end
