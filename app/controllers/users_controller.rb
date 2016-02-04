1eab9d7aa8df121e857b94f385ce97f63ad0520c
Alvin <alvinchen1988@icloud.com>
2016-02-04 11:01:25 -0800

    advance till 9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user, only: [:edit, :update, :show]
 
   def show
     @user = User.find(params[:id])
@@ -57,6 +57,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in first."
         redirect_to login_url
       end
